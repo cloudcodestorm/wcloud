d997dc8b71b7bd6ff75e9f644c5beafb7d01cf00
cloudcodestorm <cloudcodestorm@gmail.com>
2014-01-19 19:37:17 +0530

    woocomerce and contact form with css

--- wp-content/themes/wcloud/footer.php
+++ wp-content/themes/wcloud/footer.php
@@ -13,7 +13,9 @@
 		</div><!-- #main -->
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<?php get_sidebar( 'main' ); ?>
-
+<div class="cfm_footer">
+<?php accua_forms_include('2'); ?>
+</div>
 			<div class="site-info">
 				<?php do_action( 'wcloud_credits' ); ?>
 				<a href="<?php echo esc_url( __( 'http://livedesignstudios.com', 'wcloud' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'wcloud' ); ?>"><?php printf( __( '2013-2015 %s', 'wcloud' ), 'Wemagin' ); ?></a>
--- wp-content/themes/wcloud/header.php
+++ wp-content/themes/wcloud/header.php
@@ -24,6 +24,7 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/custom.css" type="text/css"/>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
@@ -33,19 +34,20 @@
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
+		<div id="logo-head">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<img src="<?php echo get_template_directory_uri(); ?>/images/headers/logo.png" titel="logo" alt="logo"/>
 			</a>
-
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e( 'Menu', 'wcloud' ); ?></h3>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'wcloud' ); ?>"><?php _e( 'Skip to content', 'wcloud' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
+					
 				</nav><!-- #site-navigation -->
 			</div><!-- #navbar -->
+</div>
+			
 		</header><!-- #masthead -->
-
+<div id="bgbanner"></div>
 		<div id="main" class="site-main">
