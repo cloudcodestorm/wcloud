63ff0194df280f7335b16432ffab22a2f352cb5d
cloudcodestorm <cloudcodestorm@gmail.com>
2014-01-19 21:09:03 +0530

    staging 19012014 9 pm

--- wp-content/themes/wcloud/header.php
+++ wp-content/themes/wcloud/header.php
@@ -35,19 +35,23 @@
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
 		<div id="logo-head">
+		<div class="logo-placeholder">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/headers/logo.png" titel="logo" alt="logo"/>
-			</a>
-			<div id="navbar" class="navbar">
+			</a></div>
+			<div id="banner_title">World's Most Affordable Cloud Backup</div>
+			<div id="banner_subtitle">With a Mission to Helping Children</div>
+
+</div>
+					<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e( 'Menu', 'wcloud' ); ?></h3>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'wcloud' ); ?>"><?php _e( 'Skip to content', 'wcloud' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 					
 				</nav><!-- #site-navigation -->
-			</div><!-- #navbar -->
-</div>
-			
+			</div><!-- #navbar -->	
+			</div><!-- #navbar -->	
 		</header><!-- #masthead -->
 <div id="bgbanner"></div>
 		<div id="main" class="site-main">
