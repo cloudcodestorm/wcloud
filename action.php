792b05041b665d27bbe603c3abf425cea99cb209
cloudcodestorm <cloudcodestorm@gmail.com>
2014-01-25 00:45:10 +0530

    serial key features

---
+++ action.php
@@ -0,0 +1,15 @@
+<?php
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+global $wpdb;
+
+$textdata = $_REQUEST['keytext'];
+$rs = $wpdb->get_results('select * from wp_serialkey where key_once like "%'.$textdata.'"'); 
+//$user_db=$rs[0]->team_name;
+$myvarResults = $wpdb->num_rows;
+if($myvarResults == 0){
+    wp_redirect( "/?page_id=137" );
+}else{
+    wp_redirect( "http://www.wemagin.com/register.php" );
+	//print('wrong');
+}
+?>
